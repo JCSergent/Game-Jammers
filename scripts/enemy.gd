909867de8130930d2962b52cedc69ea03d50c7b7
JC Sergent <jcsergent20@gmail.com>
2024-08-17 13:01:11 -0400

    Wandering

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,10 @@
-extends Area3D
+class_name Enemy extends Area3D
 
-@onready var camera = $"../../Camera3D"
 
 var in_range = false
 
 func _ready():
-	camera.released.connect(func(): if in_range: queue_free())
+	SignalBus.released.connect(func(): if in_range: queue_free())
 
 func _on_area_entered(area):
 	in_range = true
