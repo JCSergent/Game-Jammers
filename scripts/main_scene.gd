feac4318ec3ee808fbf9921eb47b0700e474e38b
Ben Sheppard <benmsheppard@gmail.com>
2024-08-18 16:05:48 -0600

    music :)

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
+@onready var music = $music
 @onready var main_camera: MainCamera = $"Main Camera"
 @onready var spawner_manager = $"boat/Enemy Spawners"
 @onready var start_enemy = $"boat/Start Enemy"
 @onready var zone = $"../impact_zone"
+@onready var enemies = $boat/enemies
+@export var play_music = false
 
 var game_state = 'Start'
 
 func _ready():
-	pass
+	if play_music:
+		music.playing = true
 	
 func _process(delta):
 	if game_state == 'Start':
@@ -18,6 +22,11 @@ func _process(delta):
 			game_state = 'Play'
 	elif game_state == 'Play':
 		pass
+	# speed up audio for more enemies
+	music.pitch_scale = min(1.8, 1 + 0.025*int(enemies.get_child_count() / 5))
+	if !music.playing and play_music:
+		music.playing = true
+		
 		#if Input.is_action_just_pressed("Q"):
 			#main_camera.view_start()
 			#spawner_manager.stop()
